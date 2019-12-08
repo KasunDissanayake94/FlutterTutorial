3173f53dc3fa7e2aa3542f313d9a425e8c6a67bb
kasund <kasund@hsenidlanka.com>
2019-12-08 17:24:33 +0530

    Drop Down box and items added

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,9 @@ class StatefullWidgetExample extends StatefulWidget{
 
 }
 class _StatefullWidgetExample extends State<StatefullWidgetExample>{
+  var _currencies  = ["Ruppees", "Dolars", "Pounds"];
   String interest = "";
+  var _currentItemSelected = "Ruppees";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +37,30 @@ class _StatefullWidgetExample extends State<StatefullWidgetExample>{
                 });
               },
             ),
+
+
+            DropdownButton<String>(
+              items: _currencies.map((String dropDownStringItem){
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              }).toList(),
+
+              onChanged: (String newValueSelected){
+                setState(() {
+                  this._currentItemSelected = newValueSelected;
+                });
+              },
+              value: _currentItemSelected,
+            ),
             Padding(
               padding: EdgeInsets.all(30.0),
               child:
               Text(
                 "You have typed  $interest",
                 style: TextStyle(
-                    fontSize: 20.0
+                    fontSize: 15.0
                 ),
               ),)
           ],
