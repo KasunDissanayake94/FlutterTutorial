f2670977c9c24243bab24138cb0fcdd9019efc92
kasund <kasund@hsenidlanka.com>
2019-12-08 11:15:53 +0530

    List View added for small List

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,38 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build returns a widget
-    return HomeScreen();
+    return MaterialApp(
+      title: "Functions",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Functions"),),
+        body: getListView(),
+      ),
+    );
   }
 
+}
+
+Widget getListView(){
+
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.edit),
+        title: Text("Edit "),
+        subtitle: Text("Edit your details"),
+      ),
+      ListTile(
+        leading: Icon(Icons.add),
+        title: Text("Add "),
+        subtitle: Text("Add your details"),
+      ),
+      ListTile(
+        leading: Icon(Icons.delete),
+        title: Text("Delete "),
+        subtitle: Text("Delete your details"),
+      )
+    ],
+  );
+
+  return listView;
 }
